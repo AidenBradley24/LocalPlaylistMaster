345a0a01222283472fc8920d06fb9588c1ecc22c
Aiden Bradley <116838707+AidenBradley24@users.noreply.github.com>
2024-05-10 17:45:10 -0400

    attempting auto version script

---
+++ autoversion.py
@@ -0,0 +1,15 @@
+import os
+
+file = open("version.txt")
+version = file.readline()
+version = version.strip()
+version_name = f"{version} Release"
+
+os.environ["MY_VERSION"] = version
+os.environ["MY_VERSION_NAME"] = version_name
+
+github_file = os.getenv("GITHUB_ENV")
+if github_file is not None:
+    with open(github_file, "a") as file:
+        file.write(f"MY_VERSION={version}")
+        file.write(f"MY_VERSION_NAME={version_name}")
